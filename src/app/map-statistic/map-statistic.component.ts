b3fb58329194ee80b1e5421fd1b3c5b33961f8f5
jthao14 <jthao1495@gmail.com>
2018-02-15 01:02:24 -0800

    Added bootstrap to Le Overwatch project and a button to go back on page for map-statistics.

--- src/app/map-statistic/map-statistic.component.ts
+++ src/app/map-statistic/map-statistic.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-map-statistic',
@@ -11,7 +12,8 @@ export class MapStatisticComponent implements OnInit {
   mapName: String;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private location: Location
   ) { }
 
   ngOnInit() {
@@ -25,5 +27,10 @@ export class MapStatisticComponent implements OnInit {
     this.mapName = this.route.snapshot.paramMap.get('mapName');
     // Makes the first character uppercase.
     this.mapName = this.mapName.charAt(0).toUpperCase() + this.mapName.slice(1);
+  }
+
+  // Goes back a page.
+  goBack() {
+    this.location.back();
   }
 }
