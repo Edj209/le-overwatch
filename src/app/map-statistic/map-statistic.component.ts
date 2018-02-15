a45fefdb0712ca6fd2af08e66c20dda4c61b027a
jthao14 <jthao1495@gmail.com>
2018-02-15 00:10:24 -0800

    Changed favicon and made the map-statistic page show the map name a little better :).

--- src/app/map-statistic/map-statistic.component.ts
+++ src/app/map-statistic/map-statistic.component.ts
@@ -21,6 +21,9 @@ export class MapStatisticComponent implements OnInit {
   // Gets the name of the map.
   // In the future it will grab data from a Mock JSON until an API is ready for it.
   getMapStatistics() {
+    // Gets the name from the url paramEter mapName.
     this.mapName = this.route.snapshot.paramMap.get('mapName');
+    // Makes the first character uppercase.
+    this.mapName = this.mapName.charAt(0).toUpperCase() + this.mapName.slice(1);
   }
 }
